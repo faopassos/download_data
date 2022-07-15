659ebdb28c549c8500fde5d32a974b701d167239
faopassos <fauez.passos@outlook.com>
2022-07-15 09:58:17 -0300

    add requirements and improve makedir

--- mag_download.py
+++ mag_download.py
@@ -3,21 +3,20 @@ import requests
 import wget
 import os
 
+
 url = 'https://embracedata.inpe.br/magnetometer/CXP/2022/'
 # by month
-ext = ['jun.22m', 'jul.22m']
+exts = ['jun.22m', 'jul.22m']
 
-print(os.getcwd)
-os.makedirs(os.getcwd() + '/data/magnetometer/')
+data_dir = os.getcwd() + '/data/magnetometer/'
+os.makedirs(data_dir, exist_ok=True)
 
 def listFD(url, ext=''):
-    page = requests.get(url).text
-    soup = BeautifulSoup(page, 'html.parser')
-    return [url + node.get('href') for node in soup.find_all('a') if node.get('href').endswith(ext)]
+  page = requests.get(url).text
+  soup = BeautifulSoup(page, 'html.parser')
+  return [url + node.get('href') for node in soup.find_all('a') if node.get('href').endswith(ext)]
 
-
-for extn in ext:
-  for file in listFD(url, extn):
+for ext in exts:
+  for file in listFD(url, ext):
     print(file)
-    wget.download(file, './data/magnetometer/')
-
+    wget.download(file, data_dir)
