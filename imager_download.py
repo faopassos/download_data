33bb80b6999fbd89fe0beb3c9f3b0ad4e38f3096
faopassos <fauez.passos@outlook.com>
2022-07-15 14:11:51 -0300

    add imager script

---
+++ imager_download.py
@@ -0,0 +1,22 @@
+from bs4 import BeautifulSoup
+import requests
+import wget
+import os
+
+
+url = 'https://embracedata.inpe.br/imager/CP/2022/CP_2022_0303/'
+# by month
+exts = ['13.tif', '00.tif']
+
+data_dir = os.getcwd() + '/data/imager/'
+os.makedirs(data_dir, exist_ok=True)
+
+def listFD(url, ext=''):
+  page = requests.get(url).text
+  soup = BeautifulSoup(page, 'html.parser')
+  return [url + node.get('href') for node in soup.find_all('a') if node.get('href').endswith(ext)]
+
+for ext in exts:
+  for file in listFD(url, ext):
+    print(file)
+    wget.download(file, data_dir)
