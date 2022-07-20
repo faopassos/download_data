29065d5cc9ab8babd1dcd3e8dffbb87094c52f66
faopassos <fauez.passos@outlook.com>
2022-07-20 09:19:26 -0300

    change imager

--- imager_download.py
+++ imager_download.py
@@ -6,7 +6,7 @@ import os
 
 url = 'https://embracedata.inpe.br/imager/CP/2022/CP_2022_0303/'
 # by month
-exts = ['O6', 'O5']
+exts = ['OH', 'O5']
 
 data_dir = os.getcwd() + '/data/imager/'
 os.makedirs(data_dir, exist_ok=True)
@@ -14,7 +14,7 @@ os.makedirs(data_dir, exist_ok=True)
 def listFD(url, ext=''):
   page = requests.get(url).text
   soup = BeautifulSoup(page, 'html.parser')
-  return [url + node.get('href') for node in soup.find_all('a') if node.get('href').endswith(ext, 0, 2)]
+  return [url + node.get('href') for node in soup.find_all('a') if node.get('href').startswith(ext, 0, 2)]
 
 for ext in exts:
   for file in listFD(url, ext):
