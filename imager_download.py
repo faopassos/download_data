83a1add34d3729f07f9c7590821fb5f68092c507
faopassos <fauez.passos@outlook.com>
2022-07-21 15:14:13 -0300

    add name main in imager script

--- imager_download.py
+++ imager_download.py
@@ -12,37 +12,42 @@ stn = 'BV'
 year = '2015'
 month = '03'
 day = '15'
+filters = ['OH-DARK', 'O6-DARK', 'O5-DARK']
+
 uri = 'https://embracedata.inpe.br/imager/'
 uri_dir = stn + '/' + year + '/' + stn + '_' + year + '_' + month + day
 url = uri + uri_dir + '/'
-exts = ['OH-DARK', 'O6-DARK', 'O5-DARK']
 
 def checkURL(url, message):
-    try:
-        urllib.request.urlretrieve(url)
-    except urllib.error.HTTPError as err:
-        print(f'{err} - {message}: {url}')
-        exit()
+  try:
+    urllib.request.urlretrieve(url)
+  except urllib.error.HTTPError as err:
+      print(f'{err} - {message}: {url}')
+      exit()
 
-
-error_message = 'No data from this date or invalid date/stn'
-checkURL(url, error_message)
 
 def makeDir(path):
-    base_dir = os.getcwd()
-    os.makedirs(base_dir + '/data/' + path, exist_ok=True)
+  base_dir = os.getcwd()
+  os.makedirs(base_dir + '/data/' + path, exist_ok=True)
 
 
-def listFD(url, ext_len, ext=''):
+def listFD(url, ft_len, ft=''):
   page = requests.get(url).text
   soup = BeautifulSoup(page, 'html.parser')
-  return [url + node.get('href') for node in soup.find_all('a') if node.get('href').startswith(ext, 0, ext_len)]
+  return [url + node.get('href') for node in soup.find_all('a') if node.get('href').startswith(ft, 0, ft_len)]
 
 
-data_dir = 'imager' + '/' + uri_dir
-makeDir(data_dir)
+if __name__ == '__main__':
+  error_message = 'No data from this date or invalid date/stn'
+  checkURL(url, error_message)
 
-for ext in exts:
-  for file in listFD(url, len(ext), ext):
-    print(file)
-    wget.download(file, 'data/' + data_dir)
+  data_dir = 'imager' + '/' + uri_dir
+  makeDir(data_dir)
+
+  for ft in filters:
+    if listFD(url, len(ft), ft) == []:
+      print(f'\nNo file match with filter "{ft}"')
+    else:
+      for file in listFD(url, len(ft), ft):
+        print(f'\n{file}')
+        wget.download(file, 'data/' + data_dir)
