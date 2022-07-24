2685e5d413c8278e7e17c760af86ed8d21b4f875
faopassos <fauez.passos@outlook.com>
2022-07-24 20:22:43 -0300

    insert functions to ionossonde script - OK

--- ion_download.py
+++ ion_download.py
@@ -1,22 +1,63 @@
+#!/usr/bin/python3
+
 from bs4 import BeautifulSoup
-import requests
-import wget
-import os
+import pandas as pd
+import requests, wget, os, urllib.request, urllib.error, logging, datetime
 
 
-url = 'https://embracedata.inpe.br/ionosonde/CAJ2M/2022/014/'
-# by month
-exts = ['.RSF', '.SAO']
+stn = 'CAJ2M'
+start_date = '2021-01-01'
+end_date = '2021-01-02'
+extensions = ['.SAO', '.RSF', '.TEST']
 
-data_dir = os.getcwd() + '/data/ionosonde/'
-os.makedirs(data_dir, exist_ok=True)
+url = 'https://embracedata.inpe.br/ionosonde/'
+
+
+logging.basicConfig(
+  filename = 'ionossonde_download.log',
+  format='%(asctime)s,%(levelname)s - %(message)s',
+  datefmt='%Y-%m-%d %H:%M:%S', level=logging.INFO
+)
+
+
+def checkURL(url, message):
+  try:
+    urllib.request.urlretrieve(url)
+  except urllib.error.HTTPError as err:
+      logging.info(f'{err} - {message}: "{url}"')
+      exit()
+
+
+def returnRangeOfDates(start_date, end_date, stn):
+  range_date = pd.date_range(start=start_date, end=end_date)
+  full_uri = range_date.strftime(f'{stn}/%Y/%j/')
+  return full_uri
+
+
+def makeDir(path):
+  base_dir = os.getcwd()
+  os.makedirs(base_dir + '/data/' + path, exist_ok=True)
+
 
 def listFD(url, ext=''):
   page = requests.get(url).text
   soup = BeautifulSoup(page, 'html.parser')
   return [url + node.get('href') for node in soup.find_all('a') if node.get('href').endswith(ext)]
 
-for ext in exts:
-  for file in listFD(url, ext):
-    print(file)
-    wget.download(file, data_dir)
+
+if __name__ == '__main__':
+  range_date = returnRangeOfDates(start_date, end_date, stn)
+  for day in range_date:
+    error_message = 'No data from this date or invalid input stn/date'
+    checkURL(url + day, error_message)
+
+    data_dir = 'ionossonde' + '/' + day
+    makeDir(data_dir)
+
+    for ext in extensions:
+      if listFD(url + day, ext) == []:
+        logging.info(f'No file match with filter "{ext}" for stn/date "{day}"')
+      else:
+        for file in listFD(url + day, ext):
+          logging.info(file)
+          #wget.download(file, 'data/' + data_dir)
