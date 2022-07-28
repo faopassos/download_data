a986ef2835692aa70692ca007f34dc2076889a63
faopassos <fauez.passos@outlook.com>
2022-07-28 16:50:33 -0300

    insert file_match in callisto script - OK

--- callisto_download.py
+++ callisto_download.py
@@ -5,8 +5,8 @@ import pandas as pd
 import requests, wget, os, urllib.request, urllib.error, logging
 
 
-start_date = '2022-05-01'
-end_date = '2022-05-02'
+start_date = '2022-01-25'
+end_date = '2022-01-31'
 
 url = 'https://embracedata.inpe.br/callisto/'
 
@@ -24,9 +24,9 @@ def checkURL(url, message):
       exit()
 
 
-def returnRangeOfDates(start_date, end_date, stn):
+def returnRangeOfDates(start_date, end_date):
   range_date = pd.date_range(start=start_date, end=end_date)
-  full_uri = range_date.strftime(f'{stn}/%Y/%m/')
+  full_uri = range_date.strftime('%Y-%m-%d')
   return full_uri
 
 
@@ -35,27 +35,34 @@ def makeDir(path):
   os.makedirs(base_dir + '/data/' + path, exist_ok=True)
 
 
-def listFD(url, ext=''):
+def listFD(url, ext_len, ext=''):
   page = requests.get(url).text
   soup = BeautifulSoup(page, 'html.parser')
-  return [url + node.get('href') for node in soup.find_all('a') if node.get('href').endswith(ext)]
+  return [url + node.get('href') for node in soup.find_all('a') if node.get('href').startswith(ext, 0, ext_len)]
 
 
 def downloadFiles():
-  range_date = returnRangeOfDates(start_date, end_date, 'CXP')
+  range_date = returnRangeOfDates(start_date, end_date)
   for day in range_date:
-    error_message = 'No data from this date or invalid input stn/date'
-    checkURL(url + day, error_message)
+    year_m = day[0:4]
+    month_m = day[5:7]
+    day_m = day[8:10]
+    dir_m = f'CXP/{year_m}/{month_m}/'
+    url_m = url + dir_m
+    file_match = f'INPE_{year_m}{month_m}{day_m}'
 
-    files = listFD(url + day, '.fit')
+    error_message = 'No data from this date or invalid input stn/date'
+    checkURL(url_m, error_message)
+
+    files = listFD(url_m, len(file_match), file_match)
     if files != []:
-      data_dir = f'callisto/{day}'
+      data_dir = f'callisto/{dir_m}'
       makeDir(data_dir)
       for f in files:
         logging.info(f)
         wget.download(f, 'data/' + data_dir)
     else:
-      logging.info(f'No files match with "{files}" for stn/date "{day}"')
+      logging.info(f'No files match with "{file_match}" for stn/date "{dir_m}"')
 
 
 if __name__ == '__main__':
