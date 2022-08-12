4090b1f42755df0271c51710bd118fd1bbafd947
faopassos <fauez.passos@outlook.com>
2022-08-12 15:59:52 -0300

    insert self in checkURL function. Now return expected value

--- test_class.py
+++ test_class.py
@@ -4,7 +4,7 @@ import pandas as pd
 import requests, wget, os, urllib.request, urllib.error, logging
 
 
-class downloadData:
+class EmbraceData:
   def __init__(self, instrument, start_date, end_date):
     self.instrument = instrument
     self.start_date = start_date
@@ -17,7 +17,7 @@ class downloadData:
     datefmt='%Y-%m-%d %H:%M:%S', level=logging.INFO
     )
 
-  def checkURL(url):
+  def checkURL(self, url):
       try:
         urllib.request.urlretrieve(url)
       except urllib.error.HTTPError as err:
@@ -33,11 +33,11 @@ class downloadData:
       except:
         logging.info(f'Invalid date/range of dates!\n\nYour entry dates:\n{self.start_date}\n{self.end_date}')
 
-  def showInfos(self):
+  def getCallisto(self):
     logging.info(self.checkURL(self.base_url + self.instrument))
     logging.info(self.returnRangeOfDates())
     #info = f'{base_url}{self.instrument}/ {self.start_date} {self.end_date}'
     #if self.instrument == 'callisto':
 
-ist = downloadData('callisto', '2022-01-01', '2022-01-10')
-ist.showInfos()
+ist = EmbraceData('callisto', '2022-01-01', '2022-01-10')
+ist.getCallisto()
