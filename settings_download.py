f301daa36fb8bb0983003174658a30d2be2f0d51
faopassos <fauez.passos@outlook.com>
2022-08-16 15:17:47 -0300

    insert settings file to insert data input

--- settings_download.py
+++ settings_download.py
@@ -1,10 +1,34 @@
-#!/usr/bin/python3
+CALLISTO = {
+  #YYYY-MM-DD
+  'start_date': '2022-01-01',
+  'end_date': '2022-01-02'
+}
 
-stn = 'SMS'
-year = '2022'
-month = '03'
-day = '15'
-uri = 'https://embracedata.inpe.br/imager/'
-uri_dir = stn + '/' + year + '/' + stn + '_' + year + '_' + month + day
-url = uri + uri_dir + '/'
-#exts = ['OH-DARK', 'O6-DARK', 'O5-DARK']
+#YYYY-MM-DD
+# all available stations: 'BJL', 'BV', 'CA', 'CP', 'SMS'
+# all available filters: 'O6', 'OH', 'O6-DARK', 'OH-DARK'
+IMAGER = {
+  'start_date': '2022-01-01',
+  'end_date': '2022-01-02',
+  'stations': ['BJL', 'CA', 'CP'],
+  'filters': ['O6-DARK', 'OH-DARK']
+}
+
+#YYYY-DOY
+# all available stations: 'BLJ03', 'BVJ03', 'CAJ2M', 'CGK21', 'FZA0M', 'SAA0K', 'SMK29'
+# all available extensions: '.RSF', '.SAO', '.PNG', '.DFT', '.DVL', '.SKY' 'SAO.XML'
+IONOSONDE = {
+  'start_date': '2022-001',
+  'end_date': '2022-002',
+  'stations': ['BLJ03', 'SAA0K'],
+  'extensions': ['.SAO', '.RSF']
+}
+
+#YYYY-DOY
+# all available stations: 'ALF', 'ARA', 'CBA', 'CHI', 'CXP', 'EUS', 'JAT', 'MAN','MED',
+#                         'PAL', 'PVE', 'RGA', 'SJC', 'SLZ', 'SMS', 'STM', 'TCM', 'VSS'
+MAGNETOMETER = {
+  'start_date': '2022-01-01',
+  'end_date': '2022-01-02',
+  'stations': ['ALF', 'ARA', 'CBA']
+}
