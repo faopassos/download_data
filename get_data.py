fb0f3888b86b64d7538579c08a614c1a67d047b0
faopassos <fauez.passos@outlook.com>
2022-08-17 08:53:11 -0300

    insert name main

--- get_data.py
+++ get_data.py
@@ -1,15 +1,12 @@
+#!/usr/bin/python3
 import embracedata_download
 
-
-get_data = embracedata_download.Embrace_Data()
 
 #YYYY-MM-DD
 CALLISTO = { 
   'start_date': '2022-01-31',
   'end_date': '2022-01-31'
 }
-get_data.Callisto(CALLISTO['start_date'], CALLISTO['end_date'])
-
 
 #YYYY-MM-DD
 # all available stations: 'BJL', 'BV', 'CA', 'CP', 'SMS'
@@ -17,23 +14,19 @@ get_data.Callisto(CALLISTO['start_date'], CALLISTO['end_date'])
 IMAGER = {
   'start_date': '2022-01-01',
   'end_date': '2022-01-02',
-  'stations': ['BJL', 'CA', 'CP'],
+  'stations': ['CA', 'CP'],
   'filters': ['O6-DARK', 'OH-DARK']
 }
-get_data.Imager(IMAGER['start_date'], IMAGER['end_date'], IMAGER['stations'], IMAGER['filters'])
-
 
 #YYYY-DOY
 # all available stations: 'BLJ03', 'BVJ03', 'CAJ2M', 'CGK21', 'FZA0M', 'SAA0K', 'SMK29'
-# all available extensions: '.RSF', '.SAO', '.PNG', '.DFT', '.DVL', '.SKY' 'SAO.XML'
+# all available extensions: '.RSF', '.SAO', '.PNG', '.DFT', '.DVL', '.SKY' 'SAO.XML' 'MMM'
 IONOSONDE = {
   'start_date': '2020-036',
   'end_date': '2020-036',
   'stations': ['BLJ03', 'SAA0K'],
   'extensions': ['.SAO', '.RSF']
 }
-get_data.Ionosonde(IONOSONDE['start_date'], IONOSONDE['end_date'], IONOSONDE['stations'], IONOSONDE['extensions'])
-
 
 #YYYY-DOY
 # all available stations: 'ALF', 'ARA', 'CBA', 'CHI', 'CXP', 'EUS', 'JAT', 'MAN','MED',
@@ -43,4 +36,13 @@ MAGNETOMETER = {
   'end_date': '2022-01-10',
   'stations': ['ALF', 'ARA', 'CBA']
 }
-get_data.Magnetometer(MAGNETOMETER['start_date'], MAGNETOMETER['end_date'], MAGNETOMETER['stations'])
+
+def download_Files():
+  get_data = embracedata_download.Embrace_Data()
+  #get_data.Callisto(CALLISTO['start_date'], CALLISTO['end_date'])
+  get_data.Imager(IMAGER['start_date'], IMAGER['end_date'], IMAGER['stations'], IMAGER['filters'])
+  #get_data.Ionosonde(IONOSONDE['start_date'], IONOSONDE['end_date'], IONOSONDE['stations'], IONOSONDE['extensions'])
+  #get_data.Magnetometer(MAGNETOMETER['start_date'], MAGNETOMETER['end_date'], MAGNETOMETER['stations'])
+
+if __name__ == '__main__':
+  download_Files()
