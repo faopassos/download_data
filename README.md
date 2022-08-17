14a79b198b65ebdfe57ce694041a34bc55fbc365
faopassos <fauez.passos@outlook.com>
2022-08-17 10:10:20 -0300

    insert license and readme

---
+++ README.md
@@ -0,0 +1,20 @@
+
+input format:
+
+CALLISTO:
+YYYY-MM-DD
+
+IMAGER:
+YYYY-MM-DD
+all available stations: 'BJL', 'BV', 'CA', 'CP', 'SMS'
+all available filters: 'O6', 'OH', 'O6-DARK', 'OH-DARK'
+
+IONOSONDE:
+YYYY-DOY
+all available stations: 'BLJ03', 'BVJ03', 'CAJ2M', 'CGK21', 'FZA0M', 'SAA0K', 'SMK29'
+all available extensions: '.RSF', '.SAO', '.PNG', '.DFT', '.DVL', '.SKY' 'SAO.XML' 'MMM'
+
+MAGNETOMETER:
+YYYY-DOY
+all available stations: 'ALF', 'ARA', 'CBA', 'CHI', 'CXP', 'EUS', 'JAT', 'MAN','MED',
+                        'PAL', 'PVE', 'RGA', 'SJC', 'SLZ', 'SMS', 'STM', 'TCM', 'VSS'
--- get_data.py
+++ get_data.py
@@ -2,15 +2,11 @@
 import embracedata_download
 
 
-#YYYY-MM-DD
 CALLISTO = { 
   'start_date': '2022-01-31',
   'end_date': '2022-01-31'
 }
 
-#YYYY-MM-DD
-# all available stations: 'BJL', 'BV', 'CA', 'CP', 'SMS'
-# all available filters: 'O6', 'OH', 'O6-DARK', 'OH-DARK'
 IMAGER = {
   'start_date': '2022-01-01',
   'end_date': '2022-01-02',
@@ -18,9 +14,6 @@ IMAGER = {
   'filters': ['O6-DARK', 'OH-DARK']
 }
 
-#YYYY-DOY
-# all available stations: 'BLJ03', 'BVJ03', 'CAJ2M', 'CGK21', 'FZA0M', 'SAA0K', 'SMK29'
-# all available extensions: '.RSF', '.SAO', '.PNG', '.DFT', '.DVL', '.SKY' 'SAO.XML' 'MMM'
 IONOSONDE = {
   'start_date': '2020-036',
   'end_date': '2020-036',
@@ -28,9 +21,6 @@ IONOSONDE = {
   'extensions': ['.SAO', '.RSF']
 }
 
-#YYYY-DOY
-# all available stations: 'ALF', 'ARA', 'CBA', 'CHI', 'CXP', 'EUS', 'JAT', 'MAN','MED',
-#                         'PAL', 'PVE', 'RGA', 'SJC', 'SLZ', 'SMS', 'STM', 'TCM', 'VSS'
 MAGNETOMETER = {
   'start_date': '2022-01-01',
   'end_date': '2022-01-10',
