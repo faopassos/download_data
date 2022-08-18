86a648d3bf4e1863b1610e09d231544d1edeb84d
faopassos <fauez.passos@outlook.com>
2022-08-18 11:43:44 -0300

    insert lidar and scintillation function in embracedata_download - work fine

--- embracedata_download.py
+++ embracedata_download.py
@@ -23,17 +23,17 @@ class Embrace_Data:
 
   def returnRangeOfDates(self, instrument, start_date, end_date):
     try:
-      if instrument == 'callisto' or instrument == 'imager' or instrument == 'magnetometer':
+      if instrument == 'callisto' or instrument == 'imager' or instrument == 'magnetometer' or instrument == 'lidar':
         s_date = datetime.strptime(start_date, '%Y-%m-%d').date()
         e_date = datetime.strptime(end_date, '%Y-%m-%d').date()
-      elif instrument == 'ionosonde':
+      elif instrument == 'ionosonde' or instrument == 'scintillation':
         s_date = datetime.strptime(start_date, '%Y-%j').date()
         e_date = datetime.strptime(end_date, '%Y-%j').date()
       if (s_date > e_date):
         exit()
-      if instrument == 'callisto' or instrument == 'imager':
+      if instrument == 'callisto' or instrument == 'imager' or instrument == 'lidar':
         range_date = pd.date_range(start=start_date, end=end_date).strftime('%Y-%m-%d')
-      elif instrument == 'ionosonde':
+      elif instrument == 'ionosonde' or instrument == 'scintillation':
         range_s = datetime.strptime(start_date, '%Y-%j').strftime("%Y-%m-%d")
         range_e = datetime.strptime(end_date, '%Y-%j').strftime("%Y-%m-%d")
         range_date = pd.date_range(start=range_s, end=range_e).strftime('%Y-%j')
@@ -47,7 +47,7 @@ class Embrace_Data:
   def listFiles(self, instrument, url, ext='', ext_len=0):
     page = requests.get(url).text
     soup = BeautifulSoup(page, 'html.parser')
-    if instrument == 'callisto' or instrument == 'imager':
+    if instrument == 'callisto' or instrument == 'imager' or instrument == 'lidar' or instrument == 'scintillation':
       return [url + node.get('href') for node in soup.find_all('a') if node.get('href').startswith(ext, 0, ext_len)] 
     elif instrument == 'ionosonde' or instrument == 'magnetometer':
       return [url + node.get('href') for node in soup.find_all('a') if node.get('href').endswith(ext)]
@@ -133,6 +133,35 @@ class Embrace_Data:
           else:
             logging.info(f'No file match with extension "{ext}" for stn/date "{uri}"')
 
+  def Lidar(self, start_date, end_date, elements):
+    range_date = self.returnRangeOfDates('lidar', start_date, end_date)
+    for element in elements:
+      for rd in range_date:
+        year = rd[0:4]
+        month = rd[5:7]
+        day = rd[8:10]
+        uri = f'lidar/{year}/{year}{month}{day}/{element}/'
+        full_url = self.base_url + uri
+        file_match = f'SJC_LID01_L0_{element[0:1]}RW_STP_V01_{year}{month}{day}'
+        self.checkURL(full_url)
+
+        files = self.listFiles('lidar', full_url, file_match, len(file_match))
+        if files != []:
+          local_file_path = f'{os.getcwd()}/data/' + uri
+          os.makedirs(local_file_path, exist_ok=True)
+          for file in files:
+            file_exists = file.rsplit('/', 1)[1]
+            if not os.path.exists(local_file_path + file_exists):
+              try:
+                logging.info(f'Downloading {file}')
+                wget.download(file, local_file_path)
+              except:
+                logging.info(f'Something went wrong with file "{file}". Please try again later.')
+            else:
+              logging.info(f'File "{file_exists}" already downloaded.')
+        else:
+          logging.info(f'No files match with "{file_match}" for stn/date "{uri}"')
+  
   def Magnetometer(self, start_date, end_date, stations):
     for stn in stations:
       range_date = self.returnRangeOfDates('magnetometer', start_date, end_date)
@@ -159,3 +188,33 @@ class Embrace_Data:
             logging.info(f'No file match with name "{file_match}" for stn/date "{full_url}"')
         else:
           logging.info(f'File "{file_match}" already downloaded.')
+
+  def Scintillation(self, start_date, end_date, stations):
+    for stn in stations:
+      range_date = self.returnRangeOfDates('scintillation', start_date, end_date)
+      for rd in range_date:
+        rd_formated = datetime.strptime(rd, '%Y-%j').strftime("%Y-%m-%d")
+        year = rd[0:4]
+        doy = rd[5:8]
+        month = rd_formated[5:7]
+        day = rd_formated[8:10]
+        uri = f'scintillation/{year}/{doy}/{stn}/'
+        file_match = f'{stn}_{year}-{month}-{day}'
+        self.checkURL(self.base_url + uri)
+
+        files = self.listFiles('scintillation', self.base_url + uri, file_match, len(file_match))
+        if files != []:
+          local_file_path = f'{os.getcwd()}/data/' + uri
+          os.makedirs(local_file_path, exist_ok=True)
+          for file in files:
+            file_exists = file.rsplit('/', 1)[1]
+            if not os.path.exists(local_file_path + file_exists):
+              try:
+                logging.info(f'Downloading file {file}')
+                wget.download(file, local_file_path)
+              except:
+                logging.info(f'Something went wrong with file "{file}". Please try again later.')
+            else:
+              logging.info(f'File "{file_exists}" already downloaded.') 
+        else:
+          logging.info(f'No file match with "{file_match}" for stn/date "{uri}"')
--- get_data.py
+++ get_data.py
@@ -20,10 +20,22 @@ IONOSONDE = {
   'extensions': ['.SAO']
 }
 
+LIDAR = {
+  'start_date': '2022-07-28',
+  'end_date': '2022-07-30',
+  'elements': ['Potassium', 'Sodium']
+}
+
 MAGNETOMETER = {
   'start_date': '2022-02-03',
   'end_date': '2022-02-05',
   'stations': ['ALF', 'ARA']
+}
+
+SCINTILLATION = {
+  'start_date': '2021-046',
+  'end_date': '2021-047',
+  'stations': ['imp', 'ios']
 }
 
 def download_Files():
@@ -31,7 +43,9 @@ def download_Files():
   #get_data.Callisto(**CALLISTO)
   #get_data.Imager(**IMAGER)
   #get_data.Ionosonde(**IONOSONDE)
+  #get_data.Lidar(**LIDAR)
   #get_data.Magnetometer(**MAGNETOMETER)
+  get_data.Scintillation(**SCINTILLATION)
 
 if __name__ == '__main__':
   download_Files()
